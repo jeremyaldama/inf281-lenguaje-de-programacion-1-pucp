1f5c983f9d5a8bed4dae5ae8fcf853a15497dfcd
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2022-12-07 21:54:05 -0500

    ejemplo clase 04

---
+++ 01 Ejemplos Clases/C4 - cin y cout/FuncionesAuxiliares.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void ImprimirLinea(char c, int n){
+    for (int i=0; i<n; i++) cout.put(c);
+    cout<<endl;
+}
---
+++ 01 Ejemplos Clases/C4 - cin y cout/FuncionesAuxiliares.h
@@ -0,0 +1,6 @@
+#ifndef funcionesauxiliares_h
+#define funcionesauxiliares_h
+
+void ImprimirLinea(char, int);
+
+#endif
