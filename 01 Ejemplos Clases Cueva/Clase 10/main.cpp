e3d043419231b80deb8f6521259c5f5e5ca4542c
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-04-25 16:45:05 -0500

    clase 10

---
+++ 01 Ejemplos Clases Cueva/Clase 10/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeremy Aldama 20206228
+ *
+ * Created on 19 de abril de 2023, 10:35
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
