e3288632e6f999eb24403f43b9f2410910bbf35d
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2023-05-06 13:01:35 -0500

    lab5 22.2

---
+++ 02 Soluciones Evaluaciones/LAB5/L5_22_2/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeremy Aldama 20206228
+ *
+ * Created on 6 de mayo de 2023, 12:50
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
